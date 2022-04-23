8043daaf95e9d784f4f12a7da16c702e6e4489c4
Misigomartin <Misigomartin@gmail.com>
2022-04-23 22:28:33 +0300

    added a method

--- user.py
+++ user.py
@@ -1,7 +1,10 @@
 
 class User:
+    user_list=[]
     def __init__(self,first_name,last_name,phone_number,email):
         self.first_name=first_name
         self.last_name=last_name
         self.phone_number=phone_number
         self.email=email
+
+    def 
