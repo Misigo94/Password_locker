ad6d4d5b470fcd44aff135eafae4d5fa22c76ac7
Misigomartin <Misigomartin@gmail.com>
2022-04-24 11:47:11 +0300

    add doc strings

--- user.py
+++ user.py
@@ -1,6 +1,10 @@
 
 class User:
+    '''
+    class that creates new instances of users
+    '''
     user_list=[]
+
     def __init__(self,first_name,last_name,phone_number,email):
         self.first_name=first_name
         self.last_name=last_name
@@ -8,8 +12,11 @@ class User:
         self.email=email
 
     def save_user(self):
+        '''
+        functions that saves user objects to the user_list
+        '''
         User.user_list.append(self)
 
-    def delete__user(self):
-        User.user_list.remove(self)
+    # def delete__user(self):
+    #     User.user_list.remove(self)
         
